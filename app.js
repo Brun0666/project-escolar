a2cb1f02ebf0fd8caaf14cffec7d857afb193d43
Bruno Gabriel <bruno.valerio.viana@escola.pr.gov.br>
2025-10-23 09:00:26 -0300

    jrlghrushgduioatga

--- app.js
+++ app.js
@@ -1,7 +1,100 @@
-let titulo = document.querySelector('h1')
-titulo.innerHTML = 'jogo do numero secreto';
+// let titulo = document.querySelector('h1')
+// titulo.innerHTML = 'jogo do numero secreto';
 
 let paragrafo = document.querySelector('p')
 paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
 
-function verificarChute()
+function exibirTextoNaTela() {
+let campo = document.querySelector(tag)
+campo.innerHTML = texto;
+}
+
+
+function verificarChute() {
+    console.log('o botão foi clicado!');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
